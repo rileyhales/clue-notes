a4c8c7dfe37f19a380a2a063ca06060ea6783b22
Riley Hales <rileyhales1@gmail.com>
2021-10-17 17:18:22 -0600

    set /clue-notes homepage

--- src/MarkerBox.js
+++ src/MarkerBox.js
@@ -9,8 +9,10 @@ const MarkerBox = (props) => {
             switch (currentCheckState) {
                 case "":
                     return "X"
-                case "X":
+                case "?":
                     return "O"
+                case "X":
+                    return ""
                 case "O":
                     return ""
                 default:
